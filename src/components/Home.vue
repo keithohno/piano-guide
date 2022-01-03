1d2b0982aa0cb2aa09a3e60c88163d3b2e8312d6
Keith Zhang <keithohno@gmail.com>
2022-01-03 13:17:08 -0800

    Added Links to Home Page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,32 @@
 <template>
   <p>Home Page</p>
+  <div v-for="(link, i) in links" :key="i">
+    <router-link :to="'/' + link">
+      {{ names[i] }}
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data: function () {
+    return {
+      links: [
+        "notes",
+        "practice_notes",
+        "steps_sharps_flats",
+        "scales",
+        "keys",
+      ],
+      names: [
+        "The Notes of the Piano",
+        "Practice 1",
+        "Steps, Sharps, and Flats",
+        "Scales",
+        "Keys",
+      ],
+    };
+  },
 };
 </script>
